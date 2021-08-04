895905f7bacb961012014ef79ca95416923ff3d2
Kevin Chu <kevin.git.chu@gmail.com>
2021-08-03 18:50:42 -0700

    Used gson to parse YouTube Data API JSON data

--- app/src/main/java/com/kchew/youtubeamanuensis/MainActivity.kt
+++ app/src/main/java/com/kchew/youtubeamanuensis/MainActivity.kt
@@ -7,25 +7,29 @@ import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.api.services.youtube.model.CaptionListResponse
+import com.google.gson.GsonBuilder
 import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private val YOUTUBE_API_KEY = "AIzaSyCp8VekQxGvqntgWqhLCzcVZbcK2mQps3w"
     private val CHANNEL_ID = "UCDKZ01hZBrN4KjCoWM1W5wA"
-    private val CHANNEL_URL =
+    /*private val CHANNEL_URL =
         "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.captions.list?" +
                 "part=snippet" +
                 "&videoId=liJVSwOiiwg" +
-                "&maxResults=20" + "&key=" + YOUTUBE_API_KEY
+                "&maxResults=20" + "&key=" + YOUTUBE_API_KEY*/
+    private val CHANNEL_URL = "https://www.googleapis.com/youtube/v3/captions?videoId=LXLDLzdj-gk&part=snippet&key=$YOUTUBE_API_KEY"
     private val client = OkHttpClient()
+    private var goddamnresponse : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //run(CHANNEL_URL)
-        runVolleyRequest(this)
+        val response = run(CHANNEL_URL)
+        //runVolleyRequest(this)
     }
 
     private fun runVolleyRequest(context: Context) {
@@ -33,20 +37,30 @@ class MainActivity : AppCompatActivity() {
         val url =
             "https://www.googleapis.com/youtube/v3/captions?videoId=LXLDLzdj-gk&part=snippet&key=$YOUTUBE_API_KEY"
         val stringRequest = StringRequest(com.android.volley.Request.Method.GET, url,
-            com.android.volley.Response.Listener<String> { response ->
-                findViewById<TextView>(R.id.TextView1).text = "Response is: ${response.substring(0,500)}"
+            { response ->
+                findViewById<TextView>(R.id.TextView1).text = "Response is: $response"
             },
-            com.android.volley.Response.ErrorListener { findViewById<TextView>(R.id.TextView1).text =  "That did not work!"})
+            { findViewById<TextView>(R.id.TextView1).text =  "That did not work!"})
 
         q.add(stringRequest)
     }
 
-    private fun run(url: String) {
+    private fun run(url: String) : String? {
         val request = Request.Builder().url(url).build()
 
         client.newCall(request).enqueue(object: Callback {
             override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+            override fun onResponse(call: Call, response: Response) {
+                //var body = response.body()?.string()
+                goddamnresponse = response.body?.string()
+                //if(body != null) findViewById<TextView>(R.id.TextView1).text = "Response is: $body"
+                //println(response.body()?.string())
+            }
         })
+
+        val gson = GsonBuilder().create()
+        val r = gson.fromJson(goddamnresponse, CaptionListResponse::class.java)
+
+        return goddamnresponse
     }
 }
